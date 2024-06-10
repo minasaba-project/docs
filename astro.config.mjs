ef1dbc367df6b20b922a6bcf28ad992e2a6e8d08
コマリン親衛隊 <masato190411@icloud.com>
2024-06-10 12:55:58 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://docs.minasaba.jp',
-	base: 'docs',
 	integrations: [
 		starlight({
 			title: 'Minasaba Docs',
