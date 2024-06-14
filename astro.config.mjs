37bb7da1ebb576ca60f897eb5bd43717c9813c41
maamokun/MikanDev <maamo-kun@outlook.com>
2024-06-15 00:19:47 +0900

    feat: CSS + join page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,28 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://docs.minasaba.jp',
-	integrations: [
-		starlight({
-			title: 'Minasaba Docs',
-			social: {
-				github: 'https://github.com/minasaba-project',
-			},
-			sidebar: [
-				{
-					label: "法的事項",
-					autogenerate: {
-						directory: "legal",
-					},
-				},
-			],
-		}),
-	],
+  site: 'https://docs.minasaba.jp',
+  integrations: [starlight({
+    title: 'Minasaba Docs',
+    favicon: "/favicon.png",
+    social: {
+      github: 'https://github.com/minasaba-project'
+    },
+    customCss: ["./src/tailwind.css"],
+    sidebar: [{
+      label: "基本情報",
+      autogenerate: {
+        directory: "general"
+      }
+    }, {
+      label: "法的事項",
+      autogenerate: {
+        directory: "legal"
+      }
+    }]
+  }), tailwind()]
 });
